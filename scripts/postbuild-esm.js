523c3971a03ec93f90a89b201ebfe2bebcc96ad9
Chlod Alejandro <chlod@chlod.net>
2024-02-29 17:54:40 +0800

    esm: fix relative imports issue

--- scripts/postbuild-esm.js
+++ scripts/postbuild-esm.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires,security/detect-non-literal-fs-filename */
 const fs = require( 'fs' );
 const path = require( 'path' );
 
@@ -6,11 +6,30 @@ const esmDirectory = path.join(
 	process.env.npm_config_local_prefix || process.cwd(), 'dist', 'esm'
 );
 
-// Well-known execution path
-// eslint-disable-next-line security/detect-non-literal-fs-filename
+function processImports( directory ) {
+	const entries = fs.readdirSync( directory, { withFileTypes: true } );
+
+	entries.forEach( entry => {
+		const entryPath = path.join( directory, entry.name );
+
+		if ( entry.isDirectory() ) {
+			processImports( entryPath );
+		} else if ( entry.isFile() ) {
+			console.log( entry );
+			let content = fs.readFileSync( entryPath, 'utf-8' );
+
+			const importRegex = /((?:ex|im)port .+? from\s*['"])(\..*?)(?<!\.js)(['"])/g;
+			content = content.replace( importRegex, '$1$2.js$3' );
+
+			fs.writeFileSync( entryPath, content );
+		}
+	} );
+}
+
 if ( fs.existsSync( esmDirectory ) ) {
-	// eslint-disable-next-line security/detect-non-literal-fs-filename
 	fs.writeFileSync( path.join( esmDirectory, 'package.json' ), JSON.stringify( {
 		type: 'module'
 	} ) );
+
+	processImports( esmDirectory );
 }
