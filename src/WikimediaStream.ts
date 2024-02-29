03a4ffe44e369a33c889923833e4c1291c377c13
Chlod Alejandro <chlod@chlod.net>
2024-02-29 17:01:34 +0800

    3.0.0-rc0

--- src/WikimediaStream.ts
+++ src/WikimediaStream.ts
@@ -369,7 +369,7 @@ export class WikimediaStream extends EventEmitter {
 	/**
 	 * The version of this package. Used for the default `User-Agent` header.
 	 */
-	static readonly VERSION = '2.1.0';
+	static readonly VERSION = '3.0.0-rc0';
 
 	/**
 	 * The generic user agent to be used by wikimedia-streams when
